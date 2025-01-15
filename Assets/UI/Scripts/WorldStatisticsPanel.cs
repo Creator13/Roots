2ed07f202148f080b9d3a69af482d43ddbd42ed6
Casper van Battum <cvanbattum@gmail.com>
2025-01-15 15:03:45 +0100

    stats qol

--- Assets/UI/Scripts/WorldStatisticsPanel.cs
+++ Assets/UI/Scripts/WorldStatisticsPanel.cs
@@ -1,12 +1,18 @@
-﻿using Roots.World;
+﻿using System;
+using Roots.World;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace Roots.UI
 {
     public class WorldStatisticsPanel : MonoBehaviour
     {
+        [SerializeField] private bool showStatistics = true;
+        
         [Header("UI elements")]
+        [SerializeField] private RectTransform statsRootPanel;
         [SerializeField] private TMP_Text objectCountText;
         [SerializeField] private TMP_Text chunkLoaderText;
         
@@ -16,6 +22,25 @@ namespace Roots.UI
         
         private void Update()
         {
+            if (Keyboard.current.ctrlKey.isPressed && Keyboard.current.f9Key.wasPressedThisFrame)
+            {
+                showStatistics = !showStatistics;
+                UpdateVisibility();
+            }
+            
+            if (showStatistics)
+            {
+                UpdateValues();
+            }
+        }
+
+        private void UpdateVisibility()
+        {
+            statsRootPanel.gameObject.SetActive(showStatistics);
+        }
+        
+        private void UpdateValues()
+        {
             if (pointRenderer.enabled)
             {
                 objectCountText.fontStyle = FontStyles.Normal;
@@ -23,11 +48,16 @@ namespace Roots.UI
             }
             else
             {
-                objectCountText.SetText($"0 obj");
                 objectCountText.fontStyle = FontStyles.Strikethrough;
+                objectCountText.SetText("0 obj");
             }
 
             chunkLoaderText.text = $"{chunkLoader.ActiveChunkGenJobCount} active jobs";
+        }
+
+        private void OnValidate()
+        {
+            UpdateVisibility();
         }
     }
 }
