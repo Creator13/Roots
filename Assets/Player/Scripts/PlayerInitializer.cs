975f547d84306372808407563b18051286356b04
Casper van Battum <cvanbattum@gmail.com>
2025-01-16 15:28:52 +0100

    fix spawning

--- Assets/Player/Scripts/PlayerInitializer.cs
+++ Assets/Player/Scripts/PlayerInitializer.cs
@@ -1,13 +1,19 @@
 ﻿using Roots.World;
+using StarterAssets;
 using UnityEngine;
 
 namespace Roots.Player
 {
-    public class PlayerPositionInitializer : MonoBehaviour
+    public class PlayerInitializer : MonoBehaviour
     {
         [SerializeField] private ChunkLoader chunkLoader;
-        [SerializeField] private Transform playerTransform;
-        
+        [SerializeField] private FirstPersonController playerController;
+
+        private void Awake()
+        {
+            playerController.enabled = false;
+        }
+
         private void Start()
         {
             chunkLoader.InitialChunksLoaded += PlacePlayerInValley;
@@ -18,8 +24,10 @@ namespace Roots.Player
             chunkLoader.InitialChunksLoaded -= PlacePlayerInValley;
 
             Vector3 lowestPoint = chunkLoader.FindLowestPointNearChunk(Vector2Int.zero, maxRadius: 2);
+            lowestPoint.y += 0.05f;
             Debug.Log($"Placing player at low point: {lowestPoint}");
-            playerTransform.position = lowestPoint;
+            playerController.transform.position = lowestPoint;
+            playerController.enabled = true;
         }
     }
 }
